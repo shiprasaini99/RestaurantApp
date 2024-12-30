c44619ad27868b94a012f9b36f2ddab228c08132
sainishipra99 <shiprasaini2012@gmail.com>
2024-12-30 19:37:00 +0530

    Cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './app/components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Button>Click me</Button>
