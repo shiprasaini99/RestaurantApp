02ffcdc7d6f46d4d1ea50194385667817d2933f4
sainishipra99 <shiprasaini2012@gmail.com>
2025-01-01 17:49:43 +0530

    Added Log In btn

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import MobileNav from './MobileNav'
+import MainNav from './MainNav'
 
 function Header() {
   return (
@@ -9,6 +10,7 @@ function Header() {
         <div className='container mx-auto flex justify-between items-center'>
             <Link to='/' className='text-2xl font-bold tracking-tight text-orange-500'>Veg Delights</Link>
             <div className='md:hidden'><MobileNav/></div>
+            <div className='hidden md:block'> <MainNav/></div>
         </div>
       </div>
     </div>
