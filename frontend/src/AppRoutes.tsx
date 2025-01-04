8a5d1834c573f69e1812705802c694fe5d136a00
sainishipra99 <shiprasaini2012@gmail.com>
2025-01-04 10:05:12 +0530

    HomePage code

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { to } from './../node_modules/@types/node/vm.d';
 import Layout from './layouts/layout';
+import HomePage from './pages/HomePage';
 
 function AppRoutes() {
   return (
     <Routes>
-        <Route path="/" element={<Layout>Home Page</Layout>} />
+        <Route path="/" element={<Layout><HomePage/></Layout>} />
         <Route path="/about" element={<span>About US</span>} />
         <Route path="*" element={<Navigate to="/" />} />
     </Routes>
--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "../app/components/Header";
+import Hero from "../app/components/Hero";
 
 type Props = {
     children : React.ReactNode;
@@ -8,6 +9,7 @@ const Layout = ({children} : Props) => {
     return (
         <div className="flex flex-col min-h-screen bg-gray-100">
            <Header/>
+           <Hero/>
            <div className="container mx-auto flex-1 py-10">{children}</div>
         </div>
     )
