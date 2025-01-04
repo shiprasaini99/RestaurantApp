5bb1d1e307792fcb36944ddc0b40fbb5960cecbd
sainishipra99 <shiprasaini2012@gmail.com>
2025-01-04 10:48:23 +0530

    SearchBar code

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,14 @@
 import landingImage from "../assets/landing.png";
 import playStore from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "../app/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 export default function HomePage() {
+    const navigate = useNavigate();
+    const handleSearchSubmit = (searchFormValues: SearchForm) => {
+        navigate({
+          pathname: `/search/${searchFormValues.searchQuery}`,
+        });
+      };
   return (
     <div className='flex flex-col gap-12'>
       <div className='bg-white rounded-lg shadow-md py-9 flex flex-col gap-6 text-center -mt-16'>
@@ -8,6 +16,10 @@ export default function HomePage() {
             Tuck into a takeway today
         </h1>
         <span className='text-xl'>Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
       </div>
       <div className='bg-white grid md:grid-cols-2 gap5'>
         <img src={landingImage}/>
