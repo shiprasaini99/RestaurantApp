0b99291600bd6fe40f40c05721334d4f3061b243
sainishipra99 <shiprasaini2012@gmail.com>
2025-01-19 00:21:01 +0530

    create user api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,13 @@ import cors  from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected with database"));
-
+import myUserRoute from "./routes/MyUserRoutes";
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.get("/test", async (req : Request, res : Response) => {
- res.json({ message: "Hello World" });
-});
+
+app.use("/api/my/user", myUserRoute);
+
 
 app.listen(7000, () => {
  console.log("Server is running on port 7000");
