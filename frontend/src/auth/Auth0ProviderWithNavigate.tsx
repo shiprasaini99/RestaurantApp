3d141ef7be6daf48679ab20a2a5bdb935ad7227c
sainishipra99 <shiprasaini2012@gmail.com>
2025-01-19 00:57:17 +0530

    create user request

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,12 @@
 import type { User } from '@auth0/auth0-react';
 import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { useCreateMyUser } from '../api/MyUserApi';
 type Props = {
     children: React.ReactNode
 }
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+    const {createUser} = useCreateMyUser();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -16,7 +18,11 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
         throw new Error("Auth0 configuration is missing");
     }
     const onRedirectCallback = (appState?: AppState, user?:User) => {
-            console.log(appState);
+            if(user?.sub && user?.email){
+                createUser({
+                    auth0Id:user.sub,
+                    email:user.email
+            })}
             console.log("user", user);
     }
     return(
@@ -32,5 +38,6 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
         </Auth0Provider>
     )
 }
+
 
 export default Auth0ProviderWithNavigate;
